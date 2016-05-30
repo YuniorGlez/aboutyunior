6be5748b8a95e59f9247ee38e9da4d081b49f5c4
YuniorGlez <yunior.developer@hotmail.com>
2016-05-30 20:57:23 +0100

    ?

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-//app.get('/', function (req, res) {
-//	res.sendStatus(200);
-//});
+app.all('*', function (req, res, next) {
+	req.accepts('application/json');
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	res.header('Access-Control-Allow-Methods', 'GET');
+	next();
+});
+app.get('/', function (req, res) {
+	res.sendStatus(200);
+});
 app.listen(8000, function () {
 	console.log('Node app is running on port', 8000);
 });
