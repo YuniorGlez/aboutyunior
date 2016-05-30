a31dd87b79399fd5ad4c5395115cb189cb29ef1e
YuniorGlez <yunior.developer@hotmail.com>
2016-05-30 20:51:15 +0100

    ?

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.use(express.static(__dirname + '/public'));
 //app.get('/', function (req, res) {
 //	res.sendStatus(200);
 //});
-app.listen(80, function () {
-	console.log('Node app is running on port', 80);
+app.listen(8000, function () {
+	console.log('Node app is running on port', 8000);
 });
